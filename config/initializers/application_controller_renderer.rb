ca5d348c65390ece48d39e91dc04c6175cef9121
EC2 Default User <kyohe1208@gmail.com>
2021-05-06 13:29:22 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file.
+Refile.secret_key = '4377912ac604e61d1ed241ae6b7a91d1070be30aea845e53320521606b42abfdc0ff251b289c8968b60bab34ec7c798791a74571a5b9fc050a58b11a8f6211ef'
 
 # ActiveSupport::Reloader.to_prepare do
 #   ApplicationController.renderer.defaults.merge!(
